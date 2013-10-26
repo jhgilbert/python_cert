ad232c49105769fe601517c67468b29b6fc7c64f
Jen Gilbert <jgilbert@arubanetworks.com>
2013-10-26 15:40:43 -0700

    Create minimal fileops module

---
+++ python2/TestDrivenDevelopment/fileops.py
@@ -0,0 +1,12 @@
+"""Reads a list from a file and writes a list to a file."""
+
+def write_list(fn, lst):
+    """Writes a list to a named file. Each list item will be on a separate
+    line. Overwrites the file if it already exists."""
+    pass
+
+def read_list(fn, lst):
+    """Reads a list from a file without using readline. Uses standard line
+    endings ("\n") to delimit list items."""
+    pass
+
